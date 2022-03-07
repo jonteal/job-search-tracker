5b5815014f1111324a0c7836175dc0dbcb458190
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-03-06 22:40:00 -0600

    Initial form structure created. Now it needs eventhandling and state management

--- client/src/components/AddApp/AddApp.jsx
+++ client/src/components/AddApp/AddApp.jsx
@@ -1,9 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./addapp.css";
 
 const AddApp = () => {
+
+    // const handleSubmit = async (event) => {
+    //     event.preventDefault();
+
+    //     try {
+    //         const { data } = await addApp({
+    //             variables: inputForm,
+    //         })
+    //         window.location.assign('/home');
+
+    //     } catch (err) {
+    //         console.log(err);
+    //     }
+    // };
+
+
     return(
-    <div>
+    <div className="addAppContainer">
         <h1>Add an Application!</h1>
 
         {/* Form that takes in information about company they applied with.
@@ -15,6 +32,84 @@ const AddApp = () => {
         5. Contact name (if applicable)
         6. Should there be an interview requested field that they can added in later?
         6. If so, post interview notes? */}
+
+        <form className="form">
+        
+            <input 
+                className="formInput"
+                type="text" 
+                name="companyName"
+                placeholder="Company Name" 
+            />
+            
+            <br />
+
+            <input 
+                className="formInput"
+                type="text" 
+                name="position"
+                placeholder="Position" 
+            />
+            
+            <br />
+
+            {/* Data Picker input component here */}
+            <h5>Date Applied</h5>
+
+            <br />
+
+        {/* Type of work - Onsite, Remote, or Hybrid */}
+            <select>
+                <option>
+                    Type
+                </option>
+                <option>Onsite</option>
+                <option>Remote</option>
+                <option>Hybrid</option>
+            </select>
+
+            <br />
+
+            <input 
+                className="formInput"
+                type="text" 
+                name="city"
+                placeholder="City" 
+            />
+
+            <br />
+
+            <input 
+                className="formInput"
+                type="text" 
+                name="state"
+                placeholder="State" 
+            />
+
+            <br />
+
+            <input 
+                className="formInput"
+                type="text" 
+                name="payRange"
+                placeholder="Pay range (if available)" 
+            />
+
+            <br />
+
+            {/* Data Picker input component here */}
+            <h5>Follow up Date</h5>
+
+            <br />
+
+            <button className="submitBtn">
+                Submit
+            </button>
+            
+        </form>
+
+        <br />
+
 
         <div className="homeBtnContainer">
             <Link to='/home'><button className="ui primary basic button">Home</button></Link>
