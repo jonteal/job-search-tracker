9a9ec4366d57ea3d44d9556ad4fe1eafa6ed91cb
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-03-06 22:11:23 -0600

    Pseudo code/comments added to addapp component for future guidance on functionality

--- client/src/components/AddApp/AddApp.jsx
+++ client/src/components/AddApp/AddApp.jsx
@@ -6,6 +6,16 @@ const AddApp = () => {
     <div>
         <h1>Add an Application!</h1>
 
+        {/* Form that takes in information about company they applied with.
+        Fields to ask about:
+        1. Company Name
+        2. Date Applied
+        3. Remote / Onsite / Hybrid
+        4. Next Steps? Follow Up date? Should we give user option to set their follow up by a metric, such as one week out?
+        5. Contact name (if applicable)
+        6. Should there be an interview requested field that they can added in later?
+        6. If so, post interview notes? */}
+
         <div className="homeBtnContainer">
             <Link to='/home'><button className="ui primary basic button">Home</button></Link>
         </div>
