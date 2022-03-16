e98edd5ac117ab349a6f12ebd53a2de89419bc03
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-03-15 20:53:58 -0500

    Minor styling to give dashboard some color

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import './navbar.css';
 
 const Navbar = () => {
     return(
         <div>
-            <nav>
+            <nav className="navbar">
                 <h1>job hunt</h1>
                 <div className="links">
                     <Link to='/'>Home</Link>
-                    {/* <Link to='/'></Link> */}
+                    <Link to='/addapp'>Add App</Link>
                 </div>
             </nav>
         </div>
--- client/src/components/Wishlist/Wishlist.jsx
+++ client/src/components/Wishlist/Wishlist.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import './wishlist.css';
 
 const Wishlist = () => {
     return(
