e416e8fff2a6e2bd9c13827961f45ea23c0125f4
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-03-20 12:21:36 -0500

    AppDetails display when you click on App, Delete App works

--- job-search-tracker/src/AppDetails.js
+++ job-search-tracker/src/AppDetails.js
@@ -1,6 +1,29 @@
+import { useParams, useHistory } from "react-router-dom";
+import useFetch from "./useFetch";
+
 const AppDetails = () => {
+
+    const { id } = useParams();
+    const { data: app, error, isPending } = useFetch('http://localhost:8000/apps/' + id);
+    const history = useHistory();
+
+    const handleClick = () => {
+        fetch('http://localhost:8000/apps/' + app.id, {
+            method: 'DELETE'
+    }).then(() => {
+        history.push('/');
+    })
+}
     return(
         <div>
+            { isPending && <div>Loading...</div>}
+            { error && <div>{ error }</div> }
+            { app && (
+                <article>
+                    <h2>{ app.company }</h2>
+                    <button onClick={handleClick}>delete app</button>
+                </article>
+            )}
             
         </div>
     )
