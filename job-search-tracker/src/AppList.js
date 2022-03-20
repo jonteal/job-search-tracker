dedd8a6bd9d85a3b907efe9644bbd70f464f7efa
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-03-20 11:08:50 -0500

    Created hompage that will handle loading and errors until page loads

---
+++ job-search-tracker/src/AppList.js
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom';
+
+const AppList = ({ apps, company } => {
+    return(
+        <div>
+            
+        </div>
+    )
+})
+
+export default AppList;
--- job-search-tracker/src/Home.js
+++ job-search-tracker/src/Home.js
@@ -1,7 +1,13 @@
+import useFetch from "./useFetch";
+
 const Home = () => {
+
+    const { data: blogs, isPending, error } = useFetch('http://localhost:8000/apps');
     return(
-        <div>
-            
+        <div className="home">
+            { error && <div>{ error }</div>}
+            { isPending && <div>Loading...</div>}
+            { apps && <AppList apps={apps} title="All Apps!"/>}
         </div>
     )
 }
