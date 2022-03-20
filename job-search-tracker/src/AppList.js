ba010c6e8b0aeeedf83d03cde4195fd8b95d07bb
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-03-20 11:13:25 -0500

    AppList created with mapping functionality to display all current apps

--- job-search-tracker/src/AppList.js
+++ job-search-tracker/src/AppList.js
@@ -1,11 +1,19 @@
 import { Link } from 'react-router-dom';
 
-const AppList = ({ apps, company } => {
+const AppList = ({ apps, company }) => {
     return(
         <div>
-            
+            <h2>{ company }</h2>
+            {apps.map((app) => (
+                <div className='app=preview' key={app.id}>
+                    <Link to={`/apps/${app.id}`}>
+                        <h2>{ app.company }</h2>
+                    </Link>
+                </div>
+
+            ))}
         </div>
     )
-})
+}
 
 export default AppList;
