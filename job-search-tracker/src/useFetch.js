38b4611ba1de6a55db144c929f3af0a163a0308d
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-03-19 21:53:17 -0500

    Built out useState and useEffect hooks to handle fetch and clean up

--- job-search-tracker/src/useFetch.js
+++ job-search-tracker/src/useFetch.js
@@ -2,7 +2,40 @@ import { useState, useEffect } from "react";
 
 const useFetch = (url) => {
 
-    
+    const [data, setData] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
+
+
+    useEffect(() => {
+        
+        // Creating a constant that we can then associate to the fetch function in order to stop it when we do the cleanup
+        const abortCont = new AbortController();
+
+        fetch(url, { signal: abortCont.signal })
+            .then(res => {
+                if(!res.ok) {
+                    throw Error('could not fetch the data for that resource')
+                }
+                return res.json();
+            })
+            .then(data => {
+                setData(data);
+                setIsPending(false);
+                setError(null);
+            })
+            .catch(err => {
+                if (err.name === 'AbortError') {
+                    console.log('fetch aborted')
+                } else {
+                    setIsPending(false);
+                    setError(err.message);
+                }
+            })
+
+            // useEffect Cleanup function aborts with whatever fetch it is associated with
+            return() => abortCont.abort();
+    }, [url])
 
 
     return { data, isPending, error };
