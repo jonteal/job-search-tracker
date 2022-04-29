a30d3d6f42f667279f530ab4089207e78650a17b
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-04-29 10:21:18 -0500

    Requirements for index.js file created

---
+++ api/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const authRoute = require('./routes/auth');
+const userRoute = require('./routes/users');
+const applicationRoute = require('./routes/applications');
+const path = require('path');
+
