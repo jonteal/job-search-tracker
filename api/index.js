6d92fd8e6a496d5d8a44e470f5eb2d34d55c8442
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-04-29 10:26:12 -0500

    Index file build completed

--- api/index.js
+++ api/index.js
@@ -7,3 +7,23 @@ const userRoute = require('./routes/users');
 const applicationRoute = require('./routes/applications');
 const path = require('path');
 
+dotenv.config();
+app.use(express.json());
+
+mongoose
+    .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindandModify: true
+    })
+    .then(console.log('Connected to MongoDB!'))
+    .catch((err) => console.log(err));
+
+app.use('/api/auth', authRoute);
+app.use('/api/users', userRoute);
+app.use('/api/applications', applicationsRoute);
+
+app.listen('5000', () => {
+    console.log('Backend is running!');
+})
