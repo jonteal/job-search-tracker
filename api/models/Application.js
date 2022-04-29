c61299c7103236b872c1b6530ddc874feffeb666
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-04-29 18:19:50 -0500

    Application model mostly done

--- api/models/Application.js
+++ api/models/Application.js
@@ -3,27 +3,40 @@ const mongoose = require('mongoose');
 const ApplicationSchema = new mongoose.Schema(
     {
         company: {
-
+            type: String,
+            required: true,
+            unique: false,
         },
         position: {
-
+            type: String,
+            required: true,
+            unique: false,
         },
         location: {
-
+            type: String,
+            required: false,
+            unique: false,
         },
         contact: {
-
+            type: String,
+            required: false,
+            unique: false,
         },
         interviewRequested: {
-
+            type: Boolean,
+            required: false,
+            unique: false,
         },
         interviewDate: {
-
+            type: String,
+            required: false,
+            unique: false,
         },
         notes: {
-
-        }
-
+            type: String,
+            required: false,
+            unique: false,
+        },
     },
     { timestamps: true }
 );
