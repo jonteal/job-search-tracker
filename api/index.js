ecab58b27c03b9b69b6ae499a7b0bdebf9e25208
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-04-29 15:47:07 -0500

    user login and .env debugging

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
-// const authRoute = require('./routes/auth');
+const authRoute = require('./routes/auth');
 // const userRoute = require('./routes/users');
 // const applicationRoute = require('./routes/applications');
 const path = require('path');
@@ -20,7 +20,7 @@ mongoose
     .then(console.log('Connected to MongoDB!'))
     .catch((err) => console.log(err));
 
-// app.use('/api/auth', authRoute);
+app.use('/api/auth', authRoute);
 // app.use('/api/users', userRoute);
 // app.use('/api/applications', applicationsRoute);
 
---
+++ api/routes/users.js
@@ -0,0 +1,5 @@
+const router = require('express').Router();
+const User = require('../models/User');
+const Application = require('../models/Application');
+const bcrypt = require('bcrypt');
+
