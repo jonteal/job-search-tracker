fbd60fc2d95df7739d5eea742e1126c9b9ac9998
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-04-30 13:59:21 -0500

    Added Actions to Login. Going to build Context files next to match them

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,17 +1,30 @@
 import axios from 'axios';
 import { useContext, useRef } from 'react';
 import { Link } from 'react-router-dom';
-import { Context } from 'react';
+// import { Context } from 'react';
 
 export default function Login() {
     const userRef = useRef();
     const passwordRef = useRef();
     const { dispatch, isFetching } = useContext(Context);
 
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        dispatch({ type: "LOGIN_START" });
+        try {
+            const res = await axios.post('/auth/login', {
+                username: userRef.current.value,
+                password: passwordRef.current.value,
+            })
+        } catch (err) {
+            dispatch({ type: "LOGIN_FAILURE" });
+        }
+    }
+
     return(
-        <div>
-            <span>Login</span>
-            <form>
+        <div className='login'>
+            <span className='loginTitle'>Login</span>
+            <form className='loginForm' onSubmit={handleSubmit}>
                 <label>Username</label>
                 <input 
                     type='text' 
