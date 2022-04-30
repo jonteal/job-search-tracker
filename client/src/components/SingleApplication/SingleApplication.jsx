b5a374c0ac5bc854d3c07383717fe12eb2294118
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-04-30 18:10:45 -0500

    handleDelete and handleUpdate created for singleapplication.js

--- client/src/components/SingleApplication/SingleApplication.jsx
+++ client/src/components/SingleApplication/SingleApplication.jsx
@@ -6,10 +6,80 @@ import { Context } from "../../context/Context";
 // Get update and delete icons
 
 export default function SingleApplication() {
-    return(
-        <div>
-            <div>
+    const location = useLocation();
+    const path = location.pathname.split('/')[2];
+    const [application, setApplication] = useState({});
+    const { user } = useContext(Context);
 
+    const [company, setCompany] = useState('');
+    const [position, setPosition] = useState('');
+    const [dateApplied, setDateApplied] = useState('');
+    const [workLocation, setWorkLocation] = useState('');
+    const [followUpDate, setFollowUpDate] = useState('');
+    const [contactName, setContactName] = useState('');
+    const [interviewRequested, setInterviewRequested] = useState('');
+    const [interviewDate, setInterviewDate] = useState('');
+    const [notes, setNotes] = useState('');
+
+    const [updateMode, setUpdateMode] = useState(false);
+
+    useEffect(() => {
+        const getApplication = async () => {
+            const res = await axios.get('/applications/' + path);
+            setApplication(res.data);
+            setCompany(res.data.company);
+            setPosition(res.data.position);
+            setDateApplied(res.data.dateApplied);
+            setWorkLocation(res.data.workLocation);
+            setFollowUpDate(res.data.followUpDate);
+            setContactName(res.data.contactName);
+            setInterviewRequested(res.data.interviewRequested);
+            setInterviewDate(res.data.interviewDate);
+            setNotes(res.data.notes);
+        };
+        getApplication();
+    }, [path]);
+
+    const handleDelete = async () => {
+        try {
+            await axios.delete(`/applications/${application._id}`, {
+                data: { username: user.username },
+            });
+            window.location.replace('/');
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
+    const handleUpdate = async () => {
+        try {
+            await axios.put(`/applications/${application._id}`, {
+                username: user.username,
+                company,
+                position,
+                dateApplied,
+                workLocation,
+                followUpDate,
+                contactName,
+                interviewRequested,
+                interviewDate,
+                notes,
+            });
+            setUpdateMode(false)
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    return(
+        <div className="singleApplication">
+            <div className="singleApplicationWrapper">
+                {updateMode ? (
+                    <input 
+                        type="text" 
+                        value={}
+                    />
+                )}
             </div>
         </div>
     )
