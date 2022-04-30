6e3cd58db93bd43ae24e1618a222812079519f8c
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-04-30 17:44:23 -0500

    Added imports to the SingleApplication.jsx file

--- client/src/components/SingleApplication/SingleApplication.jsx
+++ client/src/components/SingleApplication/SingleApplication.jsx
@@ -1,8 +1,15 @@
+import axios from "axios"
+import { useContext, useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { Context } from "../../context/Context";
+// Get update and delete icons
+
 export default function SingleApplication() {
     return(
         <div>
             <div>
-                
+
             </div>
         </div>
     )
